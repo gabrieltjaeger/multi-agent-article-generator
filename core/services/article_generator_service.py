7eb2934c2b00e595cd2b203dda6e50a4279da9d3
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-04-13 13:38:13 -0300

    :sparkles: feat(core/services): implement `ArticleGeneratorService` for article generation

---
+++ core/services/article_generator_service.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from core.types.content_request import ContentRequest
+from core.types.content_response import ContentResponse
+
+RequestType = TypeVar("RequestType", bound=ContentRequest)
+ResponseType = TypeVar("ResponseType", bound=ContentResponse)
+
+class ArticleGeneratorService(ABC, Generic[RequestType, ResponseType]):
+    """
+    Abstract base class for generating articles.
+    """
+
+    async def execute(self, topic: RequestType) -> ResponseType:
+        """
+        Executes the article generation process.
+
+        Args:
+            topic (str): The topic to generate an article for.
+
+        Returns:
+            str: The generated article.
+        """
+        return await self._generate_article(topic)
+      
+    @abstractmethod
+    async def _generate_article(self, topic: RequestType) -> ResponseType:
+        """
+        Abstract method to generate an article based on the provided topic.
+
+        Args:
+            topic (str): The topic to generate an article for.
+
+        Returns:
+            str: The generated article.
+        """
+        pass
